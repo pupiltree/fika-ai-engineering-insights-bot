40ab2a294cd969ef1d8b97c0766bdf7597889236
Harshit <harshitnerd@gmail.com>
2025-07-03 11:01:15 +0530

    done slack

--- insight_narrator_agent.py
+++ insight_narrator_agent.py
@@ -1,4 +1,8 @@
 from state_schema import MyState
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
 
 def insight_narrator_agent(state: MyState) -> MyState:
     print("🧠 InsightNarratorAgent: generating summary...")
@@ -6,3 +10,16 @@ def insight_narrator_agent(state: MyState) -> MyState:
     additions = state["diff_summary"]["additions"]
     state["insight"] = f"This week, total churn was {churn} lines of code. {additions} lines were added."
     return state
+
+import requests
+
+def send_to_slack(insight: str, author_stats: list):
+    webhook_url = os.getenv("SLACK_WEBHOOK_URL")
+    message = f"*Weekly Insight:*\n{insight}\n\n*Author Stats:*\n" + "\n".join(
+        [f"- {a['author']}: ➕ {a['add']} | ➖ {a['del']} | 📁 {a['files']} files" for a in author_stats]
+    )
+    response = requests.post(webhook_url, json={"text": message})
+    if response.status_code != 200:
+        print("❌ Slack error:", response.text)
+    else:
+        print("📤 Insight sent to Slack.")
